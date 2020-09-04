f0d43b60d1f166d38f79e221c066a4aea718885b
SunJet Liu <sunjetliu@gmail.com>
2020-09-04 03:16:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,9 +64,7 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
+
 # }
 # return the entire updated hash
 
@@ -85,7 +83,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash =
+{
+    :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }}
 end
 
 def changing_alan
